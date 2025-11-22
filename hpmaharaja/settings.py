7357fc0bfa1d82aed1dd78b1aebf0e52794a6d2b
hpmaharaja <heran@finityone.com>
2025-11-21 23:56:37 -0800

    added wbesitE

--- hpmaharaja/settings.py
+++ hpmaharaja/settings.py
@@ -82,23 +82,11 @@ WSGI_APPLICATION = 'hpmaharaja.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-ENVIRONMENT = os.environ.get("DJANGO_ENV", "local")
-
-if ENVIRONMENT == "vercel":
-    # No real DB in production (Vercel) – dummy backend
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.dummy",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.dummy",
     }
-else:
-    # Local dev: regular SQLite
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
